477680726c519d35c0406cbf3255f743aa13bd3a
CK <ckbang7@gmail.com>
2021-03-30 01:47:39 +0900

    Update dependency
    with fixed error

--- Runtime/Modules/Status/IF_VR_StatusModules.cs
+++ Runtime/Modules/Status/IF_VR_StatusModules.cs
@@ -1,12 +1,23 @@
 ﻿using EcsRx.Infrastructure.Dependencies;
 using EcsRx.Infrastructure.Extensions;
+using System.Collections;
 
 namespace InterVR.IF.VR.Modules
 {
     public class IF_VR_StatusModules : IDependencyModule
     {
+        public IEnumerator Initialize(IDependencyContainer container)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public void Setup(IDependencyContainer container)
         {
+        }
+
+        public void Shutdown(IDependencyContainer container)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
